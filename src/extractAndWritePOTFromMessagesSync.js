78943111b648d7c75cf8eef3728820f5692f139f
Tore Hammervoll <tore.hammervoll@knowit.no>
2020-01-24 17:59:40 +0100

    Ensure output folder exists

--- src/extractAndWritePOTFromMessagesSync.js
+++ src/extractAndWritePOTFromMessagesSync.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-console */
 
 import fs from 'fs';
+import path from 'path';
+import mkdirp from 'mkdirp';
 import chalk from 'chalk';
 import * as R from 'ramda';
 import readAllMessageAsObjectSync from './readAllMessageAsObjectSync';
@@ -32,6 +34,8 @@ function extractAndWritePOTFromMessagesSync(
     ),
     // 3. String: with pot head
   )(srcPatterns, messageKey, messageContext);
+
+  mkdirp.sync(path.dirname(output)); // ensure the output folder exists
 
   // Output
   fs.writeFileSync(output, result);
